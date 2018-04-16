b908590bd1218393f4dfd673eba38e222f8e9294
thienlong0033@gmail.com <thienlong0033@gmail.com>
2018-04-16 14:47:46 +0100

    git init

---
+++ HodelManagerment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HodelManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
